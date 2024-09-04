afcd403510f48b3416655a46f06c62ab74bb803d
lilydeyoung <ldeyoung01@saintmarys.edu>
2024-09-04 17:02:33 -0400

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,9 @@
+#the authors' names are: Colleen and Lily
+x=3
+print(x)
+y=x*5 + x
+print ("The calculated amount is ", y)
+print(int(False))
+print(float(True))
+z=1.2
+print(int(z))
---
+++ L4.py
@@ -0,0 +1,9 @@
+#the authors' names are: Colleen and Lily
+import math
+r = 4
+circle = math.pi * r ** 2
+length = 8 / math.sqrt (2)
+square = length * length
+area = circle - square
+print (area)
+
